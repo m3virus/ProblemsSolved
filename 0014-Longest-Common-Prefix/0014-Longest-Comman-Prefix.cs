963902b4caf8780caa4d8114a76b281aaee54ccf
mohammad_hasan <mryazdani98@outlook.com>
2024-05-19 09:20:51 +0330

    Solving problem number 14

---
+++ 0014-Longest-Common-Prefix/0014-Longest-Comman-Prefix.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Questions
+{
+    public class Code
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            bool check = false;
+            var minValue = strs.MinBy(s => s.Length);
+            do
+            {
+                if (minValue.Length == 0)
+                {
+                    check = false;
+                    break;
+                }
+
+                if (Check(strs, minValue) == strs.Length)
+                {
+                    check = true;
+                    break;
+                }
+                minValue = minValue.Remove(minValue.Length - 1);
+            } while (minValue.Length != 0);
+            if (check == true)
+            {
+                return minValue;
+            }
+
+            return "";
+
+        }
+
+        public int Check(string[] strs, string minValue)
+        {
+            int check = 0;
+            string count;
+            foreach (var counter in strs)
+            {
+                count = counter.Remove(minValue.Length);
+                if (count == minValue)
+                {
+                    check++;
+                }
+            }
+            return check;
+        }
+    }
+}
---
+++ 0014-Longest-Common-Prefix/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Questions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase(new string[]{"abb", "abcfhdtj", "abd"},"ab")]
+        [TestCase(new string[]{"flower", "flow", "flight"}, "fl")]
+        [TestCase(new string[]{"reflower", "flower", "flow"}, "")]
+
+        public void IsCodeWork(string[] test, string answer)
+        {
+            var trying = new Code().LongestCommonPrefix(test);
+
+            Assert.That(trying, Is.EqualTo(answer));
+
+        }
+
+
+
+    }
+}

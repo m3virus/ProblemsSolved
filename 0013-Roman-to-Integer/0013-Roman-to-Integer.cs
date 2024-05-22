47d6080a54173a3188df419394c624b5a38c8f52
mohammad_hasan <mryazdani98@outlook.com>
2024-05-22 18:05:46 +0330

    p

---
+++ 0013-Roman-to-Integer/0013-Roman-to-Integer.cs
@@ -0,0 +1,112 @@
+﻿namespace Questions
+{
+    public class Code
+    {
+        public int RomanToInt(string s)
+        {
+            s = s.ToUpper();
+            var sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'I')
+                {
+                    if (i + 1 < s.Length)
+                    {
+
+                        if (s[i + 1] == 'V')
+                        {
+                            sum += 4;
+                            i++;
+                        }
+                        else if (s[i + 1] == 'X')
+                        {
+                            sum += 9;
+                            i++;
+                        }
+                        else
+                        {
+                            sum += 1;
+                        }
+                    }
+                    else
+                    {
+                        sum += 1;
+                    }
+                }
+                else if (s[i] == 'X')
+                {
+                    if (i + 1 < s.Length)
+                    {
+
+                        if (s[i + 1] == 'L')
+                        {
+                            sum += 40;
+                            i++;
+                        }
+                        else if (s[i + 1] == 'C')
+                        {
+                            sum += 90;
+                            i++;
+                        }
+                        else
+                        {
+                            sum += 10;
+                        }
+                    }
+                    else
+                    {
+                        sum += 10;
+                    }
+                }
+                else if (s[i] == 'C')
+                {
+                    if (i + 1 < s.Length)
+                    {
+
+                        if (s[i + 1] == 'D')
+                        {
+                            sum += 400;
+                            i ++;
+                        }
+                        else if (s[i + 1] == 'M')
+                        {
+                            sum += 900;
+                            i ++;
+                        }
+                        else
+                        {
+                            sum += 100;
+                        }
+                    }
+                    else
+                    {
+                        sum += 100;
+                    }
+                }
+                else if (s[i] == 'M')
+                {
+
+                    sum += 1000;
+
+                }
+                else if (s[i] == 'D')
+                {
+
+                    sum += 500;
+
+                }
+                else if (s[i] == 'L')
+                {
+
+                    sum += 50;
+
+                }
+                else if (s[i] == 'V')
+                {
+                    sum += 5;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ 0013-Roman-to-Integer/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Questions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase("III",3)]
+        [TestCase("LVIII", 58)]
+        [TestCase("MCMXCIV", 1994)]
+
+        public void IsCodeWork(string test, int answer)
+        {
+            var trying = new Code().RomanToInt(test);
+
+            Assert.That(trying, Is.EqualTo(answer));
+
+        }
+
+
+
+    }
+}

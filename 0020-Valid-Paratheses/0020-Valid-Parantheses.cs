1f1d2b894a41317727d494dc0a2c3ac990d71db5
mohammad_hasan <mryazdani98@outlook.com>
2024-05-22 18:03:20 +0330

    Solving Question number 20

---
+++ 0020-Valid-Paratheses/0020-Valid-Parantheses.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Questions
+{
+    public class Code
+    {
+        public bool IsValid(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i+1>=s.Length)
+                {
+                    return false;
+                }
+                if (s[i] == '(' && s[i + 1] == ')')
+                {
+                    s = s.Replace("()", "");
+                    if (s.Length == 0)
+                    {
+                        return true;
+                    }
+
+                    return IsValid(s);
+
+
+                }
+                if (s[i] == '[' && s[i + 1] == ']')
+                {
+                    s = s.Replace("[]", "");
+                    if (s.Length == 0)
+                    {
+                        return true;
+                    }
+
+                    return IsValid(s);
+                }
+                if (s[i] == '{' && s[i + 1] == '}')
+                {
+                    s = s.Replace("{}", "");
+                    if (s.Length == 0)
+                    {
+                        return true;
+                    }
+
+                    return IsValid(s);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 0020-Valid-Paratheses/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Questions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase("(", false)]
+        [TestCase("({)", false)]
+        [TestCase("({)}", false)]
+        [TestCase("{[]}", true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("({[]})", true)]
+
+        public void IsCodeWork(string test, bool answer)
+        {
+            var trying = new Code().IsValid(test);
+
+            Assert.That(trying, Is.EqualTo(answer));
+
+        }
+
+
+
+    }
+}

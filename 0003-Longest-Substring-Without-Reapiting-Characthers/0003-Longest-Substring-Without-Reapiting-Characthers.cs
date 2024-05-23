7359fcc00ad13ecb8ba34f0d424308230ffc6151
mohammad_hasan <mryazdani98@outlook.com>
2024-05-23 16:09:15 +0330

    Solving Question number 3

---
+++ 0003-Longest-Substring-Without-Reapiting-Characthers/0003-Longest-Substring-Without-Reapiting-Characthers.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Questions
+{
+    public class Code
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            var substringList = new List<string>();
+            while (s.Length > 0)
+            {
+                var subString = SubStringBuilder(s);
+                substringList.Add(subString);
+                s = s.Remove(0, 1);
+            }
+
+            return substringList.Select(x => x.Length ).Max();
+
+        }
+
+        public string SubStringBuilder(string s)
+        {
+            string builder = "";
+            foreach (var counter in s)
+            {
+                if (!builder.Contains(counter))
+                {
+                    builder += counter;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return  builder;
+        }
+    }
+}
---
+++ 0003-Longest-Substring-Without-Reapiting-Characthers/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Questions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        [TestCase("", 0)]
+        [TestCase("a", 1)]
+        //[TestCase("({[]})", true)]
+
+        public void IsCodeWork(string test, int answer)
+        {
+            var trying = new Code().LengthOfLongestSubstring(test);
+
+            Assert.That(trying, Is.EqualTo(answer));
+
+        }
+
+
+
+    }
+}

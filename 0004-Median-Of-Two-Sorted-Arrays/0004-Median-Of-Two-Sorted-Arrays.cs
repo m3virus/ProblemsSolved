21c3f1f692e4962e43f80e89b516c8d90bdf0881
mohammad_hasan <mryazdani98@outlook.com>
2024-05-25 10:56:50 +0330

    adding question number 4

---
+++ 0004-Median-Of-Two-Sorted-Arrays/0004-Median-Of-Two-Sorted-Arrays.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Text;
+using System.Xml.XPath;
+
+namespace Questions
+{
+    public class Code
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var numbers = new int[nums1.Length + nums2.Length];
+
+            if (nums1.Length == 0 && nums2.Length == 0)
+            {
+                return 0;
+            }
+            if (nums1.Length >= nums2.Length)
+            {
+
+                if (numbers.Length % 2 == 1)
+                {
+                    int i = 0;
+                    int j = 0;
+                    int index = 0;
+                    while (index <= numbers.Length / 2)
+                    {
+                        if (i >= nums1.Length)
+                        {
+                            numbers[index] = nums2[j];
+                            j++;
+                        }
+                        else if (j >= nums2.Length)
+                        {
+                            numbers[index] = nums1[i];
+                            i++;
+                        }
+                        else if (nums1[i] <= nums2[j])
+                        {
+                            numbers[index] = nums1[i];
+                            i++;
+                        }
+                        else
+                        {
+                            numbers[index] = nums2[j];
+                            j++;
+                        }
+
+                        index++;
+                    }
+
+                    return numbers[(numbers.Length - 1) / 2];
+
+                }
+                else
+                {
+                    int i = 0;
+                    int j = 0;
+                    int index = 0;
+                    while (index <= (numbers.Length + 1) / 2)
+                    {
+                        if (i >= nums1.Length )
+                        {
+                            numbers[index] = nums2[j];
+                            j++;
+                        }
+                        else if (j >= nums2.Length)
+                        {
+                            numbers[index] = nums1[i];
+                            i++;
+                        }
+                        else if (nums1[i] <= nums2[j])
+                        {
+                            numbers[index] = nums1[i];
+                            i++;
+                        }
+                        else
+                        {
+                            numbers[index] = nums2[j];
+                            j++;
+                        }
+                        index++;
+                    }
+
+                    var nu1 = Convert.ToDouble(numbers[(numbers.Length / 2) - 1]);
+                    var nu2 = Convert.ToDouble(numbers[numbers.Length / 2]);
+                    var result = (nu1 + nu2) / 2;
+                    return result;
+                }
+
+            }
+            if (nums1.Length < nums2.Length)
+            {
+
+                if (numbers.Length % 2 == 1)
+                {
+                    int i = 0;
+                    int j = 0;
+                    int index = 0;
+                    while (index <= numbers.Length / 2)
+                    {
+                        if (i >= nums2.Length)
+                        {
+                            numbers[index] = nums1[j];
+                            j++;
+                        }
+                        else if (j >= nums1.Length)
+                        {
+                            numbers[index] = nums2[i];
+                            i++;
+                        }
+                        else if (nums2[i] <= nums1[j])
+                        {
+                            numbers[index] = nums2[i];
+                            i++;
+                        }
+                        else
+                        {
+                            numbers[index] = nums1[j];
+                            j++;
+                        }
+                        index++;
+                    }
+
+                    return numbers[(numbers.Length - 1) / 2];
+
+                }
+                else
+                {
+                    int i = 0;
+                    int j = 0;
+                    int index = 0;
+                    while (index <= (numbers.Length + 1) / 2)
+                    {
+                        if (i >= nums2.Length)
+                        {
+                            numbers[index] = nums1[j];
+                            j++;
+                        }
+                        else if (j >= nums1.Length)
+                        {
+                            numbers[index] = nums2[i];
+                            i++;
+                        }
+                        else if (nums2[i] <= nums1[j])
+                        {
+                            numbers[index] = nums2[i];
+                            i++;
+                        }
+                        else
+                        {
+                            numbers[index] = nums1[j];
+                            j++;
+                        }
+
+                        index++;
+                    }
+                    var nu1 = Convert.ToDouble(numbers[(numbers.Length / 2) - 1]);
+                    var nu2 = Convert.ToDouble(numbers[numbers.Length / 2]);
+                    var result = (nu1 + nu2) / 2;
+                    return result;
+                }
+
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 0004-Median-Of-Two-Sorted-Arrays/Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Questions
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        [TestCase(new int[]{1,1,3},new int[]{2,3,4}, 2.5)]
+        [TestCase(new int[]{1,3},new int[]{2,3,4}, 3)]
+        [TestCase(new int[]{1,1,3},new int[]{2,3,4,5}, 3)]
+        [TestCase(new int[]{1,2},new int[]{3,4}, 2.5)]
+        [TestCase(new int[]{},new int[]{1}, 1)]
+        [TestCase(new int[]{1},new int[]{}, 1)]
+        [TestCase(new int[]{},new int[]{}, 0)]
+        
+
+        public void IsCodeWork(int[] test1 ,int[] test2, double answer)
+        {
+            var trying = new Code().FindMedianSortedArrays(test1, test2);
+
+            Assert.That(trying, Is.EqualTo(answer));
+
+        }
+
+
+
+    }
+}
